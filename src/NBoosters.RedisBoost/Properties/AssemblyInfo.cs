78904180dca641b122ed65a53dd7e1cc01325233
NBooster <mail.andrey.bulygin@gmail.com>
2013-03-15 18:05:04 +0200

    license terms

--- src/NBoosters.RedisBoost/Properties/AssemblyInfo.cs
+++ src/NBoosters.RedisBoost/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NBooster")]
 [assembly: AssemblyProduct("NBoosters.RedisBoost")]
-[assembly: AssemblyCopyright("Copyright © NBooster")]
+[assembly: AssemblyCopyright("Copyright 2013 Andrey Bulygin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.5.1")]
+[assembly: AssemblyFileVersion("1.5.1")]
