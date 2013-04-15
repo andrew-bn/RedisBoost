c484748afe3adfe8cae37c6579414bd77e714cd9
andrew-bn <mail.andrey.bulygin@gmail.com>
2013-04-15 12:39:50 +0300

    console test, v1.5.6

--- tests/NBoosters.RedisBoost.ConsoleLoadTest/Properties/AssemblyInfo.cs
+++ tests/NBoosters.RedisBoost.ConsoleLoadTest/Properties/AssemblyInfo.cs
@@ -5,25 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NBoosters.RedisBoost")]
-[assembly: AssemblyDescription("Lightweight asynchronous Redis client")]
+[assembly: AssemblyTitle("NBoosters.RedisBoost.ConsoleLoadTest")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NBooster")]
-[assembly: AssemblyProduct("NBoosters.RedisBoost")]
-[assembly: AssemblyCopyright("Copyright 2013 Andrey Bulygin")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("NBoosters.RedisBoost.ConsoleLoadTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: InternalsVisibleTo("NBoosters.RedisBoost.Tests")]
-[assembly: InternalsVisibleTo("Moq")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4685261e-065d-4f26-9b32-c5bbbc16d6db")]
+[assembly: Guid("66614cb5-25b9-44d6-99a8-d977dd3fbfc1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.6")]
-[assembly: AssemblyFileVersion("1.5.6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
