5b0fe901efd1937b875db2815a75b9027ac6ff5e
andrew-bn <mail.andrey.bulygin@gmail.com>
2013-05-20 12:24:30 +0300

    delete usings

--- tests/NBoosters.RedisBoost.ConsoleBenchmark/Payloads.cs
+++ tests/NBoosters.RedisBoost.ConsoleBenchmark/Payloads.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NBoosters.RedisBoost.ConsoleBenchmark
+﻿namespace NBoosters.RedisBoost.ConsoleBenchmark
 {
 	public static class Payloads
 	{
