3cc4999757ddde830d3555a18c5e200291221aab
andrew-bn <mail.andrey.bulygin@gmail.com>
2015-08-01 20:37:48 +0300

    simple console client for redis server

---
+++ RedisBoost.Cli/CommandDescriptor.cs
@@ -0,0 +1,14 @@
+﻿namespace RedisBoost.Cli
+{
+	public class CommandDescriptor
+	{
+		public string Name { get; private set; }
+		public object[] Arguments { get; private set; }
+
+		public CommandDescriptor(string name, params object[] arguments)
+		{
+			Name = name;
+			Arguments = arguments;
+		}
+	}
+}
