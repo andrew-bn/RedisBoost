c04b000fdf264c11f728a4de9e609403f68f7029
andrew-bn <mail.andrey.bulygin@gmail.com>
2016-01-26 14:31:46 +0200

    disable test parallelization

--- tests/RedisBoost.Test/Properties/AssemblyInfo.cs
+++ tests/RedisBoost.Test/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -21,3 +22,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("56f8ba25-b2fb-442e-a91a-3fc2178b6658")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
